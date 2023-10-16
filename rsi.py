f5d3303691568cc289131b3a61a101d44cec8f69
Alex Henry <tukkek@gmail.com>
2023-10-16 00:00:00 +0000

    Using day periods, plus general refactoring

--- rsi.py
+++ rsi.py
@@ -1,201 +1,173 @@
 #!/usr/bin/python3
-import time,threading,os,subprocess,sys,configparser
-from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QPushButton,QVBoxLayout,QRadioButton,QButtonGroup,QSystemTrayIcon,QMessageBox
-from PyQt5.QtGui import QIcon
-from pathlib import Path
+#TODO refactor modules for joystick, popup, tray...
+import time,threading,os,subprocess,sys,configparser,datetime,math,pathlib,PyQt5.QtGui,PyQt5.QtWidgets
 
-JOYSTICK='/dev/input/js0'
-LENIENCY=[.5,1,1.5,2]
-PERIODSAVE=60
-INCREMENT=60
+JOYSTICKPATH='/dev/input/js0'
 MINUTE=60
 HOUR=60*MINUTE
+INCREMENT=3*HOUR
+PERSIST=MINUTE
+PERIODS={
+  0:'Night',
+  3:'Late night',
+  6:'Morning',
+  9:'Late morning',
+  12:'Afternoon',
+  15:'Late afternoon',
+  18:'Evening',
+  21:'Late evening',
+}
+CONFIG=configparser.ConfigParser()
+INI=pathlib.Path.home()/'.pyrsi.ini'
+JOYSTICK=os.open(JOYSTICKPATH,os.O_RDONLY|os.O_NONBLOCK) if os.path.exists(JOYSTICKPATH) else False
 
-class Frame(QWidget):
-    def __init__(self):
-        global poollabel,leniency
-        super().__init__()
-        self.inittray()
-        self.setWindowTitle('PyRsi')    
-        layout=QVBoxLayout();
-        #label
-        poollabel=QLabel(describe())
-        layout.addWidget(poollabel)
-        #radio group
-        leniency=QButtonGroup(self)
-        lax=QRadioButton("Lax (2:1)")
-        leniency.addButton(lax)
-        layout.addWidget(lax)
-        normal=QRadioButton("Normal (1:1)")
-        leniency.addButton(normal)
-        layout.addWidget(normal)
-        normal.setChecked(True)
-        cautious=QRadioButton("Cautious (2:3)")
-        leniency.addButton(cautious)
-        layout.addWidget(cautious)
-        severe=QRadioButton("Severe (1:2)")
-        leniency.addButton(severe)
-        layout.addWidget(severe)
-        if 'data' in config:
-            leniency.buttons()[int(config['data']['leniency'])].setChecked(True)
-        #add / remove time
-        self.addbuton(f"Add {INCREMENT} minutes",layout,self.moretime)
-        self.addbuton(f"Remove {INCREMENT} minutes",layout,self.lesstime)
-        #hide to tray
-        self.addbuton("Hide",layout,self.totray)
-        self.setLayout(layout)
-    def moretime(self):
-        global pool
-        pool+=INCREMENT*60
-    def lesstime(self):
-        global pool
-        pool-=INCREMENT*60
-        if pool<0:
-            pool=0
-    def addbuton(self,label,layout,action):
-        b=QPushButton(label)
-        layout.addWidget(b)
-        b.clicked.connect(action)
-        return b
-    def closeEvent(self, event): #fired on window closed
-        if QMessageBox.question(self,'Exit','Are you sure you want to disable PyRsi?', QMessageBox.Yes, QMessageBox.No)==QMessageBox.Yes:
-            global terminate
-            popupthread.cancel()
-            terminate=True
-            event.accept()
-            app.quit()
-        else:
-            event.ignore()
-    def totray(self):
-        self.hide()
-    def inittray(self):
-        global tray
-        tray=QSystemTrayIcon(self)
-        tray.setIcon(QIcon(os.path.join(sys.path[0],'clock.png')))
-        tray.setVisible(True)
-        tray.activated.connect(self.activate)
-    def activate(self):
-        self.show()
-        self.center()
-    def center(self):
-        frameGm = self.frameGeometry()
-        d=QApplication.desktop()
-        screen = d.screenNumber(d.cursor().pos())
-        centerPoint = d.screenGeometry(screen).center()
-        frameGm.moveCenter(centerPoint)
-        self.move(frameGm.topLeft())
-    def getleniency(self):
-        return leniency.buttons().index(leniency.checkedButton())
+class Frame(PyQt5.QtWidgets.QWidget):
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle('RSI')  
+    l=PyQt5.QtWidgets.QVBoxLayout();
+    self.pool=PyQt5.QtWidgets.QLabel(describe())
+    l.addWidget(self.pool)
+    self.create(f"Rest more",l,lambda:self.rest(+INCREMENT))
+    self.create(f"Rest less",l,lambda:self.rest(-INCREMENT))
+    self.create("Hide",l,self.hide)
+    self.setLayout(l)
+    
+  def rest(self,amount):
+    global pool
+    pool+=amount
+    if pool<0:
+      pool=0
+      
+  def create(self,label,layout,action):
+    b=PyQt5.QtWidgets.QPushButton(label)
+    layout.addWidget(b)
+    b.clicked.connect(action)
+    return b
+    
+  def closeEvent(self, event):
+    prompt='Are you sure you want to disable PyRsi?'
+    y=PyQt5.QtWidgets.QMessageBox.Yes
+    if PyQt5.QtWidgets.QMessageBox.question(self,'Exit',prompt,y,PyQt5.QtWidgets.QMessageBox.No)!=y:
+      event.ignore()
+      return
+    global terminate
+    popupthread.cancel()
+    terminate=True
+    event.accept()
+    app.quit()
+      
+  def activate(self):
+    self.show()
+    self.center()
+    
+  def center(self):
+    frameGm=self.frameGeometry()
+    d=PyQt5.QtWidgets.QApplication.desktop()
+    screen=d.screenNumber(d.cursor().pos())
+    c=d.screenGeometry(screen).center()
+    frameGm.moveCenter(c)
+    self.move(frameGm.topLeft())
 
-joystick=os.open(JOYSTICK,os.O_RDONLY|os.O_NONBLOCK) if os.path.exists(JOYSTICK) else False
 lastjoystick=time.time()
 joystickthread=False
-
-app = QApplication(sys.argv)
-window = False
-poollabel=False
-
+app=PyQt5.QtWidgets.QApplication(sys.argv)
+window=False
 pool=0
 popupthread=False
 terminate=False
-leniency=False
 lastupdate=False
 lastsave=False
 tray=False
 lastpopup=False
 
-config=configparser.ConfigParser()
-ini=Path.home() / '.pyrsi.ini'
-
 def watchjoystick():
-    global lastjoystick
-    while True:
-        if terminate:
-            return
-        try:
-            os.read(joystick,8)
-            lastjoystick=time.time()
-        except:#no new data to read
-            time.sleep(1)
+  global lastjoystick
+  while True:
+    if terminate:
+      return
+    try:
+      os.read(JOYSTICK,8)
+      lastjoystick=time.time()
+    except:#no new data to read
+      time.sleep(1)
 
 def update():
-    if terminate:
-        return
-    global pool,lastupdate,lastsave
-    now=time.time()
-    if lastupdate!=False and now-lastupdate>=10:#resume from suspension
-        pool-=now-lastupdate
-    lastupdate=now
-    idle=True
-    if now-lastjoystick<=1:
-        idle=False
-    else:
-        idle=int(subprocess.Popen('xprintidle', stdout=subprocess.PIPE).stdout.read()[:-1])>1000
-    if idle:
-        pool-=1
-    else:
-        pool+=LENIENCY[window.getleniency()]
-    if pool<0:
-        pool=0
-    text=describe()
-    poollabel.setText(text)
-    tray.setToolTip(text)
-    threading.Timer(1,update).start()
-    if lastsave==False:
-        lastsave=now
-    elif now>=lastsave+PERIODSAVE:
-        lastsave=now
-        saveconfig()
+  if terminate:
+    return
+  global pool,lastupdate,lastsave
+  now=time.time()
+  if lastupdate!=False and now-lastupdate>=10:#resume from INI
+    pool-=now-lastupdate
+  lastupdate=now
+  idle=True
+  if now-lastjoystick<=1:
+    idle=False
+  else:
+    idle=int(subprocess.Popen('xprintidle', stdout=subprocess.PIPE).stdout.read()[:-1])>1000
+  pool+=-1 if idle else +1
+  if pool<0:
+    pool=0
+  text=describe()
+  window.pool.setText(text)
+  tray.setToolTip(text)
+  if lastsave==False:
+    lastsave=now
+  elif now>=lastsave+PERSIST:
+    lastsave=now
+    persist()
+  threading.Timer(1,update).start()
+
+def toperiod(datetime):
+  return PERIODS[3*math.floor(datetime.hour/3)]
 
 def describe():
-    if pool==0:
-        return 'All rested up!'
-    if pool>=60*60:
-        return 'Rest for '+str(round(pool/(HOUR)))+' hour(s)'
-    if pool>=60:
-        return 'Rest for '+str(round(pool/MINUTE))+' minute(s)'
-    return 'Rest for '+str(round(pool))+' second(s)'
+  now=datetime.datetime.now()
+  until=now+datetime.timedelta(seconds=pool)
+  now=toperiod(now)
+  until=toperiod(until)
+  return 'All rested up!' if now==until else f'Rest until {until.lower()}.'
 
 def popup():
-    global lastpopup
-    if terminate:
-        return
-    d=describe()
-    print(d)
-    p=pool
-    while p>=60:
-        p/=60
-    p=round(p)
-    if (p<10 or p%10==0) and d!=lastpopup:
-        lastpopup=d
-        tray.showMessage('PyRsi',d,msecs=5*1000)
-    setpopup()
+  if terminate:
+    return
+  global lastpopup,popupthread
+  d=describe()
+  print(d)
+  if d!=lastpopup:
+    lastpopup=d
+    tray.showMessage('PyRsi',d,msecs=5*1000)
+  popupthread=threading.Timer(HOUR,popup)
+  popupthread.start()
+  
+def restore():
+  CONFIG.read(INI)
+  if 'data' in CONFIG:
+    data=CONFIG['data']
+    global pool,lastupdate
+    pool=float(data['pool'])
+    lastupdate=float(data['lastupdate'])
     
-def setpopup():
-    global popupthread
-    popupthread=threading.Timer(10*MINUTE if pool<MINUTE else MINUTE,popup)
-    popupthread.start()
-    
-def loadconfig():
-    config.read(ini)
-    if 'data' in config:
-        data=config['data']
-        global pool,lastupdate
-        pool=float(data['pool'])
-        lastupdate=float(data['lastupdate'])
-        
-def saveconfig():
-    config['data']={}
-    config['data']['pool']=str(pool)
-    config['data']['lastupdate']=str(lastupdate)
-    config['data']['leniency']=str(window.getleniency())
-    config.write(open(ini,'w'))
-    
-if joystick!=False:
-    joystickthread=threading.Thread(target=watchjoystick)
-    joystickthread.start()
-loadconfig()
+def persist():
+  CONFIG['data']={
+    'pool':str(pool),
+    'lastupdate':str(lastupdate)
+  }
+  CONFIG.write(open(INI,'w'))
+  
+def minimize():
+  global tray
+  tray=PyQt5.QtWidgets.QSystemTrayIcon(window)
+  tray.setIcon(PyQt5.QtGui.QIcon(os.path.join(sys.path[0],'clock.png')))
+  tray.setVisible(True)
+  tray.activated.connect(window.activate)
+
+if JOYSTICK!=False:
+  joystickthread=threading.Thread(target=watchjoystick)
+  joystickthread.start()
+restore()
 window=Frame()
-setpopup()
+minimize()
+popup()
 threading.Timer(1,update).start()
 sys.exit(app.exec_())
